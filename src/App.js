f74edfded9ed6f42dcbd72cd8adfb103f7a13d39
Aniksaha-ui <sahaanik1045@gmail.com>
2022-06-09 22:39:15 +0600

    pagination done with style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import CountriesTables from "./Components/Country/CountriesTables";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  const getCountries = async () => {
-    try {
-      const response = await axios.get("https://restcountries.com/v2/all");
-      setCountries(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
   return (
     <>
       <div className="App">
-        <h1>React Datatable -{countries.length}</h1>
         <CountriesTables />
       </div>
     </>
--- src/Components/Country/CountriesTables.js
+++ src/Components/Country/CountriesTables.js
@@ -1,9 +1,81 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 const CountriesTables = () => {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState([]);
+  const getCountries = async () => {
+    try {
+      const response = await axios.get("https://restcountries.com/v2/all");
+      setCountries(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const columns = [
+    {
+      name: "country name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Country Native Name",
+      selector: (row) => row.nativeName,
+      sortable: true,
+    },
+    {
+      name: "Country Capital",
+      selector: (row) => row.capital,
+      sortable: true,
+    },
+    {
+      name: "Country Flag",
+      selector: (row) => (
+        <img width={50} height={50} src={row.flag} alt="country flag" />
+      ),
+    },
+    {
+      name: "Action",
+      cell: (row) => (
+        <button
+          onClick={() => {
+            alert(row.alpha2Code);
+          }}
+          className="btn-primary"
+        >
+          Show more
+        </button>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <div>
-      <DataTable />
+      <h1>React Datatable -{countries.length}</h1>
+      <DataTable
+        title="All countries over the world"
+        columns={columns}
+        data={countries}
+        pagination
+        fixedHeader
+        fixedHeaderScrollHeight="450px"
+        highlightOnHover
+        subHeader
+        subHeaderComponent={
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="form-control w-25"
+            type="text"
+            placeholder="Enter anything"
+          />
+        }
+      />
     </div>
   );
 };
